a1f61f2a68a08d80cca725a853283d7963acfa1f
Nils Knappmeier <n.knappmeier@i-views.de>
2015-05-22 23:35:44 +0200

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * multilang-extract-comments <https://github.com/nknapp/multilang-extract-comments>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function multilangExtractComments () {
+  // body
+}
---
+++ spec/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * multilang-extract-comments <https://github.com/nknapp/multilang-extract-comments>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var multilangExtractComments = require('../')
+
+describe('multilang-extract-comments:', function () {
+  // body
+})
